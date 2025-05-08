e1a0180d594f55a05535cff93b8e7d840fc94132
Ryan Ye <ryanye@Ryans-MacBook-Pro.local>
2025-05-08 14:31:15 -0700

    add notification context

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -1,5 +1,6 @@
 import { useAuthContext } from '@/contexts/AuthenticationContext';
 import { CameraContextProvider } from '@/contexts/CameraContext'
+import { NotificationContextProvider } from '@/contexts/NotificationContext';
 import { Redirect, Stack, useNavigation } from 'expo-router'
 import React, { useEffect } from 'react'
 import Toast from 'react-native-toast-message'
@@ -18,9 +19,11 @@ export default function _layout() {
     }
     
     return (
-        <CameraContextProvider>
-            <Stack />
-            <Toast />
-        </CameraContextProvider>
+        <NotificationContextProvider>
+            <CameraContextProvider>
+                <Stack />
+                <Toast />
+            </CameraContextProvider>
+        </NotificationContextProvider>
     )
 }
